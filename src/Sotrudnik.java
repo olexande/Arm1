9c0d616ebe5c7c355518378e9e678ba35179c74d
olexande bilovol <olexande@pisem.net>
2011-12-12 13:53:16 +0200

    v001

---
+++ src/Sotrudnik.java
@@ -0,0 +1,112 @@
+
+
+/**
+ *
+ * @author a.belovol
+ */
+
+import java.text.Collator;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Sotrudnik /*implements Comparable*/ {
+    // ID сотрудника.
+    private int personID;
+    // Имя сотрудника
+    private String personName;
+    // Фамилия сотрудника
+    private String personSurName;
+    // Отчество сотрудника
+    private String personPatronymicName;
+    
+    // поле ПОЛ
+    private char sex;
+    
+    // поле ИД Отдела
+    private int otdelID;
+    
+        // поле ДАТА РОЖДЕНИЯ
+    private Date dateOfBirth;
+    
+    /* Лишнее ... */
+    // поле ГОД ОБУЧЕНИЯ
+    private int educationYear;
+    // get/set для ГОД ОБУЧЕНИЯ
+    public int getEducationYear() {
+        return educationYear;
+    }
+    
+    // get/set для ДАТА РОЖДЕНИЯ
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+    
+    // get/set для ИД Отдела
+    public int getotdelID() {
+        return otdelID;
+    }
+    public void setDepatId(int otdelID) {
+        this.otdelID = otdelID;
+    }
+    
+    // get/set для ИД сотрудника
+    public int getPersonID() {
+        return personID;
+    }
+    public void setPersonId(int personID) {
+        this.personID = personID;
+    }
+    
+    // get/set для ИМЯ
+    public String getPersonName() {
+        return personName;
+    }
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+    
+    // get/set для ОТЧЕСТВО
+    public String getPersonPatronymicName() {
+        return personPatronymicName;
+    }
+    public void setPatronymic(String personPatronymicName) {
+        this.personPatronymicName = personPatronymicName;
+    }
+    
+    // get/set для ФАМИЛИЯ
+    public String getPersonSurName() {
+        return personSurName;
+    }
+    public void setPersonSurName(String personSurName) {
+        this.personSurName = personSurName;
+    }
+    
+    // get/set для ПОЛ
+    public char getSex() {
+        return sex;
+    }
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+    
+    
+    
+    // DateFormat - класс для преобразования даты
+    // в строку в определеннном формате.
+    // Подробнее смотрите документацию по этому методу
+    public String toString() {
+        return personName + " " + personSurName + " " + personPatronymicName + ", "
+                + DateFormat.getDateInstance(DateFormat.SHORT).format(dateOfBirth)
+                + ", ИД Отдела=" + otdelID + " Год:" + educationYear;
+    }
+
+    public int compareTo(Object obj) {
+        Collator c = Collator.getInstance(new Locale("ru"));
+        c.setStrength(Collator.PRIMARY);
+        return c.compare(this.toString(), obj.toString());
+    }
+}
